84a2c4e2ce5887ea6f42555f7440b0173ecc1b5c
NellyOgo <nellyogorodnik@gmail.com>
2023-07-16 01:00:15 +0300

    try10 Nell

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 import axios from 'axios';
 import Notiflix from 'notiflix';
 
-
 const form = document.getElementById('search-form');
 const gallery = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
@@ -24,13 +23,14 @@ function handleFormSubmit(event) {
   clearGallery();
   searchImages();
 }
+
 const lightbox = new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-    captionPosition: 'bottom',
-    captionDelay: 250,
-    animationSlide: false,
-    history: false
-  });
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  animationSlide: false,
+  history: false
+});
 
 async function searchImages() {
   const url = `https://pixabay.com/api/?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${currentPage}&per_page=40`;
@@ -69,37 +69,44 @@ function renderImages(images) {
     fragment.appendChild(card);
   });
 
-  const markup = images.map(image => {
-    return `
-    <div class="photo-card">
-      <img src="" alt="" loading="lazy" />
-      <div class="info">
-        <p class="info-item">
-          <b>Likes</b>
-        </p>
-        <p class="info-item">
-          <b>Views</b>
-        </p>
-        <p class="info-item">
-          <b>Comments</b>
-        </p>
-        <p class="info-item">
-          <b>Downloads</b>
-        </p>
-      </div>
-    </div>`
-  })
-
-  gallery.innerHTML = markup;
+  gallery.appendChild(fragment);
   lightbox.refresh();
 }
 
-// function createInfoItem(label, value) {
-//   const item = document.createElement('p');
-//   item.className = 'info-item';
-//   item.innerHTML = `<b>${label}:</b> ${value}`;
-//   return item;
-// }
+function createImageCard(image) {
+  const card = document.createElement('div');
+  card.className = 'photo-card';
+
+  const img = document.createElement('img');
+  img.src = image.webformatURL;
+  img.alt = image.tags;
+  img.loading = 'lazy';
+
+  const info = document.createElement('div');
+  info.className = 'info';
+
+  const likes = createInfoItem('Likes', image.likes);
+  const views = createInfoItem('Views', image.views);
+  const comments = createInfoItem('Comments', image.comments);
+  const downloads = createInfoItem('Downloads', image.downloads);
+
+  info.appendChild(likes);
+  info.appendChild(views);
+  info.appendChild(comments);
+  info.appendChild(downloads);
+
+  card.appendChild(img);
+  card.appendChild(info);
+
+  return card;
+}
+
+function createInfoItem(label, value) {
+  const item = document.createElement('p');
+  item.className = 'info-item';
+  item.textContent = `${label}: ${value}`;
+  return item;
+}
 
 function clearGallery() {
   gallery.innerHTML = '';
@@ -135,4 +142,3 @@ function showNotification(message) {
     Notiflix.Notify.remove();
   }, 4000);
 }
-
