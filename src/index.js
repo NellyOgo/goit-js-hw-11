b129c8d7b97207ee388043f2a253a8767e85b7a7
NellyOgo <nellyogorodnik@gmail.com>
2023-07-15 22:29:56 +0300

    try9 Nell

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const API_KEY = '38183263-7b55f24cf30c776edf1c7da27';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+import axios from 'axios';
+import Notiflix from 'notiflix';
+
 
 const form = document.getElementById('search-form');
 const gallery = document.querySelector('.gallery');
@@ -9,6 +12,7 @@ const notification = document.querySelector('.notification');
 
 let currentPage = 1;
 let searchQuery = '';
+let notificationShown = false;
 
 form.addEventListener('submit', handleFormSubmit);
 loadMoreBtn.addEventListener('click', loadMoreImages);
@@ -32,24 +36,27 @@ async function searchImages() {
   const url = `https://pixabay.com/api/?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${currentPage}&per_page=40`;
 
   try {
-    const response = await fetch(url);
-    const data = await response.json();
+    const response = await axios.get(url);
+    const data = response.data;
 
-    if (data.hits.length > 0) {
+    if (data.hits.length === 0) {
+      showNotification('Sorry, there are no images matching your search query. Please try again.');
+      return;
+    } else {
+      if (currentPage === 1) {
+        showNotification(`Hooray! We found ${data.totalHits} images.`);
+      }
       renderImages(data.hits);
       showLoadMoreBtn();
-      showNotification(`Hooray! We found ${data.totalHits} images.`);
-      scrollToNextGroup();
-    } else {
-      showNotification('Sorry, there are no images matching your search query. Please try again.');
-    }
 
-    if (data.totalHits <= currentPage * 40) {
-      hideLoadMoreBtn();
-      showNotification("We're sorry, but you've reached the end of search results.");
+      if (data.totalHits <= currentPage * 40) {
+        hideLoadMoreBtn();
+        showNotification("We're sorry, but you've reached the end of search results.");
+      }
+
+      scrollToNextGroup();
     }
   } catch (error) {
-    console.log(error);
     showNotification('An error occurred while fetching images. Please try again later.');
   }
 }
@@ -62,51 +69,37 @@ function renderImages(images) {
     fragment.appendChild(card);
   });
 
-  gallery.appendChild(fragment);
+  const markup = images.map(image => {
+    return `
+    <div class="photo-card">
+      <img src="" alt="" loading="lazy" />
+      <div class="info">
+        <p class="info-item">
+          <b>Likes</b>
+        </p>
+        <p class="info-item">
+          <b>Views</b>
+        </p>
+        <p class="info-item">
+          <b>Comments</b>
+        </p>
+        <p class="info-item">
+          <b>Downloads</b>
+        </p>
+      </div>
+    </div>`
+  })
 
-   lightbox.refresh(); 
+  gallery.innerHTML = markup;
+  lightbox.refresh();
 }
 
-function createImageCard(image) {
-  const card = document.createElement('div');
-  card.className = 'photo-card';
-
-  const imageLink = document.createElement('a');
-  imageLink.href = image.largeImageURL;
-  imageLink.setAttribute('data-lightbox', 'image');
-  card.appendChild(imageLink);
-
-  const img = document.createElement('img');
-  img.src = image.webformatURL;
-  img.alt = image.tags;
-  img.loading = 'lazy';
-  imageLink.appendChild(img);
-
-  const info = document.createElement('div');
-  info.className = 'info';
-  card.appendChild(info);
-
-  const likes = createInfoItem('Likes', image.likes);
-  info.appendChild(likes);
-
-  const views = createInfoItem('Views', image.views);
-  info.appendChild(views);
-
-  const comments = createInfoItem('Comments', image.comments);
-  info.appendChild(comments);
-
-  const downloads = createInfoItem('Downloads', image.downloads);
-  info.appendChild(downloads);
-
-  return card;
-}
-
-function createInfoItem(label, value) {
-  const item = document.createElement('p');
-  item.className = 'info-item';
-  item.innerHTML = `<b>${label}:</b> ${value}`;
-  return item;
-}
+// function createInfoItem(label, value) {
+//   const item = document.createElement('p');
+//   item.className = 'info-item';
+//   item.innerHTML = `<b>${label}:</b> ${value}`;
+//   return item;
+// }
 
 function clearGallery() {
   gallery.innerHTML = '';
@@ -137,9 +130,9 @@ function scrollToNextGroup() {
 }
 
 function showNotification(message) {
-  notification.textContent = message;
-  notification.style.display = 'block';
+  Notiflix.Notify.info(message);
   setTimeout(() => {
-    notification.style.display = 'none';
-  }, 3000);
+    Notiflix.Notify.remove();
+  }, 4000);
 }
+
